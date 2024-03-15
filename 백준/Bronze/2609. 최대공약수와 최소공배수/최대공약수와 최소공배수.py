2b672c1d471ba8565c3c7c34db53c544ce541645
cojinnam <105960247+cojinnam@users.noreply.github.com>
2024-03-15 23:50:10 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,25 +1,12 @@
 import sys
-import math
 input = sys.stdin.readline
 
 x, y = map(int, input().split())
 
-a = []
-b = []
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
 
-for i in range(1, int(math.sqrt(x))+1):
-    if x%i==0:
-        a.append(i)
-        a.append(int(x/i))
-
-for i in range(1, int(math.sqrt(y))+1):
-    if y%i==0:
-        b.append(i)
-        b.append(int(y/i))
-a.sort()
-b.sort()
-for i in range(len(a)):
-    if a[i] in b:
-        gcd = a[i]
-print(gcd)
-print(int(x*y/gcd))
+print(gcd(x, y))
+print(int(x*y/gcd(x, y)))
