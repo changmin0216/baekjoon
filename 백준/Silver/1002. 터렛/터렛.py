3239a5eeca7318836821637bde7abe902d19f536
cojinnam <105960247+cojinnam@users.noreply.github.com>
2024-03-22 15:57:43 +0900

    [Silver III] Title: 터렛, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -2,19 +2,15 @@ import sys, math
 input = sys.stdin.readline
 
 t = int(input())
+
 for _ in range(t):
     x1, y1, r1, x2, y2, r2 = list(map(int, input().split()))
-    dis = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
-    if dis == 0:  # 두 원의 중심이 같을 경우
-        if r1 == r2:  # 두 원의 크기가 같아 겹치는 경우
-            print(-1)
-        else:  # 한 원이 다른 원 안에 들어가 있는 경우
-            print(0)
-    else:  # 두 원의 중심이 다를 경우
-        if r1 + r2 == dis or abs(r2 - r1) == dis:
-            print(1)
-        elif ((abs(r1 - r2) < dis) and (dis < r1 + r2)):
-            print(2)
-        else:
-            print(0)
+    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    if distance == 0 and r1 == r2:
+        print(-1)
+    elif distance == abs(r1-r2) or distance == r1+r2:
+        print(1)
+    elif abs(r1-r2) < distance < abs(r1+r2):
+        print(2)
+    else:
+        print(0)
