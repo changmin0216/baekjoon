5bfad34af102d96d0e87e5544adcf8306db95c6b
cojinnam <105960247+cojinnam@users.noreply.github.com>
2024-03-24 14:47:22 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -3,18 +3,16 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
+chk = [False for _ in range(n+1)]
 result = []
-chk = [False]*(n+1)
 
 def recur(num):
-    if num == m:
-        for i in result:
-            print(i, end=" ")
-        print()
+    if num==m:
+        print(*result)
         return
     for i in range(1, n+1):
-        if chk[i]==False:
-            chk[i]=True
+        if chk[i] == False:
+            chk[i] = True
             result.append(i)
             recur(num+1)
             chk[i] = False
