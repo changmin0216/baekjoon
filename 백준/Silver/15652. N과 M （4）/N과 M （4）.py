290d126aa14c1c4d3af2b39e31a4dbcc60a46ecb
cojinnam <105960247+cojinnam@users.noreply.github.com>
2024-03-24 15:01:47 +0900

    [Silver III] Title: N과 M (4), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+rs = []
+def recur(start):
+    if len(rs) == m:
+        print(*rs)
+        return
+
+    for i in range(start, n+1):
+        rs.append(i)
+        recur(i)
+        rs.pop()
+
+recur(1)
