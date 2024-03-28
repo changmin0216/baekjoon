92c408cf71ea202472d7363627a14af0fec0657c
cojinnam <105960247+cojinnam@users.noreply.github.com>
2024-03-28 11:13:24 +0900

    [Silver V] Title: 집합, Time: 5920 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -7,22 +7,23 @@ m = int(input())
 
 for _ in range(m):
     com = list(map(str, input().split()))
-
-    if com[0] == 'add':
-        s.add(int(com[1]))
-    elif com[0] == 'remove':
-        s.discard(int(com[1]))
-    elif com[0] == 'check':
-        if int(com[1]) in s:
-            print(1)
+    if len(com) == 1:
+        if com[0] == 'all':
+            s = set([i for i in range(1, 21)])
         else:
-            print(0)
-    elif com[0] == 'toggle':
-        if int(com[1]) in s:
-            s.discard(int(com[1]))
-        else:
+            s = set()
+    else:
+        if com[0] == 'add':
             s.add(int(com[1]))
-    elif com[0] == 'all':
-        s = set([i for i in range(1, 21)])
-    elif com[0] == 'empty':
-        s = set()
+        elif com[0] == 'remove':
+            s.discard(int(com[1]))
+        elif com[0] == 'check':
+            if int(com[1]) in s:
+                print(1)
+            else:
+                print(0)
+        elif com[0] == 'toggle':
+            if int(com[1]) in s:
+                s.discard(int(com[1]))
+            else:
+                s.add(int(com[1]))
