b860d0dae2c3fd4387141dd56a8ab23eddb14d1b
cojinnam <105960247+cojinnam@users.noreply.github.com>
2024-04-01 20:39:03 +0900

    [level 0] Title: 문자열 출력하기, Time: 136.29 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // System.out.println(br.readLine());
+    }
+}
