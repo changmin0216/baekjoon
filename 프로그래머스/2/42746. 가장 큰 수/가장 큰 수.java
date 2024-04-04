7ef084292d3edf49ea31c1b9511702cde66df43f
cojinnam <105960247+cojinnam@users.noreply.github.com>
2024-04-04 10:16:19 +0900

    [level 2] Title: 가장 큰 수, Time: 2.54 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,20 +1,32 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
         String[] arr = new String[numbers.length];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
-        }
-        Arrays.sort(arr, ((o1, o2) -> (o2 + o1).compareTo(o1 + o2)));
+        List<Integer> arr1 = new ArrayList<>();
 
-        if (arr[0].equals("0")) {
+        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = String.valueOf(numbers[i]);
+            arr1.add(numbers[i]);
+        }
+
+//        Arrays.sort(arr, ((o1, o2) -> (o2 + o1).compareTo(o1 + o2)));
+        Collections.sort(arr1, (a, b ) -> {
+            String as = String.valueOf(a), bs = String.valueOf(b);
+            return -Integer.compare(Integer.parseInt(as + bs), Integer.parseInt(bs + as));
+        });
+
+        if (arr1.get(0).equals(0)) {
             return "0";
         }
-        StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            answer.append(arr[i]);
+
+//        if (arr[0].equals("0")) {
+//            return "0";
+//        }
+        StringBuilder sb = new StringBuilder();
+        for (Integer a : arr1) {
+            sb.append(String.valueOf(a));
         }
-        return answer.toString();
+        return sb.toString();
     }
 }
