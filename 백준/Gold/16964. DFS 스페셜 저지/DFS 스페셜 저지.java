78d9395a03050524021c923fa50fc3a2068cf979
cojinnam <105960247+cojinnam@users.noreply.github.com>
2024-04-05 17:27:33 +0900

    [Gold III] Title: DFS 스페셜 저지, Time: 564 ms, Memory: 64292 KB -BaekjoonHub

--- 백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.java
+++ 백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.java
@@ -1,77 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	static ArrayList<ArrayList<Integer>> adj;
-	static boolean[] visited;
-	static int[] answer;
-	static boolean flag;
-	static int N, idx;
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		
-		N = Integer.parseInt(br.readLine());
-		
-		adj = new ArrayList<>();
-		for(int i = 0 ; i <= N ; ++i) adj.add(new ArrayList<>());
-		
-		visited = new boolean[N + 1];
-		answer = new int[N];
-		flag = true;
-		idx = 1;
-		
-		for(int i = 0 ; i < N - 1 ; ++i) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			
-			adj.get(from).add(to);
-			adj.get(to).add(from);
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0 ; i < N ; ++i) answer[i] = Integer.parseInt(st.nextToken());
-		
-		if(answer[0] != 1) {
-			System.out.println(0);
-			return;
-		}
-		
-		dfs(1);
-		
-		if(flag) {
-			System.out.println(1);
-		} else {
-			System.out.println(0);
-		}
-		
-	}
+    static ArrayList<ArrayList<Integer>> adj;
+    static boolean flag;
+    static int n, idx;
+    static int[] answer;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException, NumberFormatException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
 
-	private static void dfs(int x) {
-		if(visited[x]) return;
-		
-		// x정점 방문
-		visited[x] = true;
-		
-		HashSet<Integer> set = new HashSet<>();
-		for(int next : adj.get(x)) {
-			if(visited[next]) continue;
-			set.add(next);
-		}
-		
-		if(set.size() == 0) return;
-		
-		if(set.contains(answer[idx])) {
-			dfs(answer[idx++]);
-		} else {
-			flag = false;
-		}
-	}
+        adj = new ArrayList<>();
+        answer = new int[n];
+        visited = new boolean[n+1];
+        flag = true;
+        idx = 1;
+
+        for (int i = 0; i <= n; ++i) {
+            adj.add(new ArrayList<Integer>());
+        }
+
+        for (int i = 0; i < n - 1; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adj.get(a).add(b);
+            adj.get(b).add(a);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            answer[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (answer[0]!=1){
+            System.out.println(0);
+            return;
+        }
+
+        dfs_solutions(1);
+
+
+        if (flag) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+    public static void dfs_solutions(int start) {
+        if (visited[start]) {
+            return;
+        }
+
+        visited[start] = true;
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int node : adj.get(start)) {
+            if (visited[node]) continue;
+            set.add(node);
+        }
+
+        if (set.size() == 0) return;
+
+        if(set.contains(answer[idx])) {
+            dfs_solutions(answer[idx++]);
+        } else {
+            flag = false;
+        }
+    }
 }
+
+
