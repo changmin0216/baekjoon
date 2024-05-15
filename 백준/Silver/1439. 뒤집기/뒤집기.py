f7d17c2a39fde5c732ec8acb787c504b61b654e8
cojinnam <105960247+cojinnam@users.noreply.github.com>
2024-05-15 11:19:43 +0900

    [Silver V] Title: 뒤집기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -6,22 +6,16 @@ ary = list(map(int, input().rstrip()))
 cnt_zero = 0
 cnt_one = 0
 
-flag = False
-for v in ary:
-    if v==0 and flag==False:
-        cnt_zero+=1
-        flag = True
+if ary[0]==1:
+    cnt_one += 1
+else:
+    cnt_zero += 1
 
-    elif v==1:
-        flag = False
-
-flag = False
-for v in ary:
-    if v==1 and flag==False:
-        cnt_one+=1
-        flag = True
-
-    elif v==0:
-        flag = False
+for i in range(len(ary)-1):
+    if ary[i] != ary[i+1]:
+        if ary[i+1] == 1:
+            cnt_one += 1
+        else:
+            cnt_zero += 1
 
 print(min(cnt_zero,cnt_one))
