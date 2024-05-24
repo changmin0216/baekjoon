81c5d8191cda4a68eb0d6758b37d8556ce797933
alsxkxk <105960247+alsxkxk@users.noreply.github.com>
2024-05-24 15:39:51 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1912 ms, Memory: 106100 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,11 +1,9 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-import heapq
-INF = int(1e9)
 
+## 도시의 개수 N, 도로의 개수 M, 거리 정보 K, 출발 도시의 번호 X
 n, m, k, x = map(int, input().split())
-
-distance = [INF for _ in range(n+1)]
 
 graph = [[] for _ in range(n+1)]
 
@@ -13,28 +11,32 @@ for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 
-def dijkstra(start):
-    distance[start] = 0
-    q = []
-    heapq.heappush(q, (0, start))
+visited = [False] * (n+1)
+
+
+def bfs(start):
+    result = []
+
+    q = deque()
+    q.append((start, 0))
+    visited[start] = True
 
     while q:
-        dist, node = heapq.heappop(q)
+        next, cost = q.popleft()
+        if cost == k:
+            result.append(next)
 
-        if distance[node] < dist:
-            continue
+        for v in graph[next]:
+            if not visited[v]:
+                visited[v] = True
+                q.append((v, cost+1))
+    return result
 
-        for i in graph[node]:
-            if distance[i] > dist+1:
-                distance[i] = dist+1
-                heapq.heappush(q, (dist+1, i))
+result = bfs(x)
+result.sort()
 
-dijkstra(x)
-flag = False
-for i in range(len(distance)):
-    if distance[i] == k:
-        print(i)
-        flag = True
-if not flag:
+if len(result) != 0:
+    for v in result:
+        print(v)
+else:
     print(-1)
-
