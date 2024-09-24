802cf00c036a7065e4fef1d0ed6c226414cda341
alsxkxk <105960247+alsxkxk@users.noreply.github.com>
2024-09-24 12:41:20 +0900

    [Gold V] Title: 내려가기, Time: 264 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+ary = list(map(int, input().split()))
+
+max_dp = ary
+min_dp = ary
+
+
+for i in range(n-1):
+    ary = list(map(int, input().split()))
+
+    # max_dp[0] = max(max_dp[0], max_dp[1]) + ary[0]
+    # min_dp[0] = min(min_dp[0], min_dp[1]) + ary[0]
+    #
+    # max_dp[1] = max(max_dp) + ary[1]
+    # min_dp[1] = min(min_dp) + ary[1]
+    #
+    # max_dp[2] = max(max_dp[1], max_dp[2]) + ary[2]
+    # min_dp[2] = min(min_dp[1], min_dp[2]) + ary[2]
+
+    max_dp = [max(max_dp[0], max_dp[1]) + ary[0], max(max_dp) + ary[1], max(max_dp[1], max_dp[2]) + ary[2]]
+    min_dp = [min(min_dp[0], min_dp[1]) + ary[0], min(min_dp) + ary[1], min(min_dp[1], min_dp[2]) + ary[2]]
+
+print(max(max_dp), min(min_dp))
