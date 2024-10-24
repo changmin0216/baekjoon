a5a4bdd6f7d127b54d589ea535524fc098fe6f97
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2024-10-24 17:02:06 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -7,36 +7,20 @@ UNION
 SELECT NAME AS 'tmp', CODE
 FROM SKILLCODES WHERE (NAME = 'C#' OR NAME = 'Python'))
 
-# SELECT CASE WHEN ((SKILL_CODE & (
-#     SELECT CODE FROM combine WHERE tmp = 'Front End'
-# ) != 0) AND (SKILL_CODE & (
-#     SELECT CODE FROM combine WHERE tmp = 'Python'
-# ) != 0)) THEN 'A' 
-# WHEN (SKILL_CODE & (
-#     SELECT CODE FROM combine WHERE tmp = 'C#'
-# ) != 0) THEN 'B'
-# WHEN (SKILL_CODE & (
-#     SELECT CODE FROM combine WHERE tmp = 'Front End'
-# ) != 0) THEN 'C' END AS 'GRADE', ID, EMAIL  
-# FROM DEVELOPERS
-# WHERE SKILL_CODE & (SELECT SUM(CODE) FROM combine) != 0 AND GRADE IS NOT NULL
-# ORDER BY 1, 2
-SELECT * 
-FROM (
-    SELECT 
-        CASE 
-            WHEN ((SKILL_CODE & (SELECT CODE FROM combine WHERE tmp = 'Front End') != 0) 
-                  AND (SKILL_CODE & (SELECT CODE FROM combine WHERE tmp = 'Python') != 0)) 
-                 THEN 'A'
-            WHEN (SKILL_CODE & (SELECT CODE FROM combine WHERE tmp = 'C#') != 0) 
-                 THEN 'B'
-            WHEN (SKILL_CODE & (SELECT CODE FROM combine WHERE tmp = 'Front End') != 0) 
-                 THEN 'C'
-            ELSE NULL
-        END AS GRADE, 
-        ID, EMAIL  
-    FROM DEVELOPERS
-    WHERE SKILL_CODE & (SELECT SUM(CODE) FROM combine) != 0
+SELECT * FROM (
+    SELECT CASE WHEN ((SKILL_CODE & (
+    SELECT CODE FROM combine WHERE tmp = 'Front End'
+) != 0) AND (SKILL_CODE & (
+    SELECT CODE FROM combine WHERE tmp = 'Python'
+) != 0)) THEN 'A' 
+WHEN (SKILL_CODE & (
+    SELECT CODE FROM combine WHERE tmp = 'C#'
+) != 0) THEN 'B'
+WHEN (SKILL_CODE & (
+    SELECT CODE FROM combine WHERE tmp = 'Front End'
+) != 0) THEN 'C' END AS 'GRADE', ID, EMAIL  
+FROM DEVELOPERS
+WHERE SKILL_CODE & (SELECT SUM(CODE) FROM combine) != 0
 ) AS subquery
 WHERE GRADE IS NOT NULL
-ORDER BY GRADE, ID;
+ORDER BY 1, 2
