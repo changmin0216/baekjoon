58b157003cd10503c1d19f3b15fafe1856782e31
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2024-10-25 11:08:21 +0900

    [Gold III] Title: 줄 세우기, Time: 196 ms, Memory: 41080 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,30 @@
+import sys
+from collections import defaultdict, deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+visited = [False] * (n+1)
+
+degree = [0 for _ in range(n+1)]
+g = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    g[a].append(b)
+    degree[b]+=1
+
+q = deque()
+for i in range(1, n+1):
+    if degree[i] == 0:
+        q.append(i)
+
+answer = []
+while q:
+    x = q.popleft()
+    answer.append(x)
+
+    for i in g[x]:
+        degree[i]-=1
+        if degree[i] == 0:
+            q.append(i)
+print(' '.join(map(str, answer)))
