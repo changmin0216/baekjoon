6de4f8355fac7f066ee1fd88ab12c764e45e8468
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2024-10-30 21:10:40 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,17 +1,24 @@
 import java.util.*;
-
 class Solution {
-    public int solution(String[][] clothes) {
-         int answer = 1;
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> hashmap = new HashMap<>();
 
-        HashMap<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
+        for (int i=0;i<clothes.length;i++){
+            if (hashmap.get(clothes[i][1])==null) {
+                hashmap.put(clothes[i][1], 1);
+            }
+            else{
+                hashmap.put(clothes[i][1], hashmap.get(clothes[i][1]) + 1);
+            }
         }
 
-        for (String key : map.keySet()) {
-            answer *= (map.get(key)+1);
+        Set<String> key = hashmap.keySet();
+
+        for (String s : key) {
+            answer *= (hashmap.get(s)+1);
         }
-        return answer-1;
+        answer-=1;
+        return answer;
     }
 }
