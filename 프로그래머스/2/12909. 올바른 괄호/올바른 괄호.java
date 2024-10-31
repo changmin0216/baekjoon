daae1d599a0da311a1d4ca0e28c323a3955c20b8
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2024-10-31 14:24:13 +0900

    [level 2] Title: 올바른 괄호, Time: 21.63 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            }
+            else{
+                if (!stack.isEmpty()) {
+                    if (stack.peek() == '(') {
+                        stack.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if (!stack.isEmpty()) {
+            answer = false;
+        }
+        return answer;
+    }
+}
