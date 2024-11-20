daccd2d6ba070fb0ff6a84dfd417ed556a9997b4
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2024-11-20 14:49:36 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 1248 ms, Memory: 210608 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,23 +1,48 @@
 import sys
+from itertools import permutations
 input = sys.stdin.readline
 
 n = int(input())
-nums = list(map(int, input().split()))
-add, sub, mul, div = map(int, input().split())
+arr = list(map(int, input().split()))
+op = list(map(int, input().split()))
+
+tmp = []
+
+o = ['+', '-', '*', '/']
+for i in range(4):
+    if op[i]!=0:
+        for _ in range(op[i]):
+            tmp.append(o[i])
+
 result = []
-ans = 0
-def cal(add, sub, mul, div, ans, i):
-    if i == n:
-        result.append(ans)
-        return
-    if add>0:
-        cal(add-1, sub, mul, div, ans+nums[i], i+1)
-    if sub>0:
-        cal(add, sub-1, mul, div, ans-nums[i], i+1)
-    if mul>0:
-        cal(add, sub, mul-1, div, ans*nums[i], i+1)
-    if div>0:
-        cal(add, sub, mul, div-1, int(ans/nums[i]), i+1)
-cal(add, sub, mul, div,  nums[0], 1)
+for l in permutations(tmp, len(tmp)):
+    answer = 0
+    for i in range(len(l)):
+        if i==0:
+            if l[i] == '+':
+                answer += arr[i]+arr[i+1]
+            elif l[i] == '-':
+                answer += arr[i] - arr[i + 1]
+            elif l[i] == '*':
+                answer += arr[i] * arr[i + 1]
+            else:
+                if arr[i] < 0 and arr[i+1] > 0:
+                    answer += -(-arr[i] // arr[i+1])
+                else:
+                    answer += arr[i] // arr[i + 1]
+        else:
+            if l[i] == '+':
+                answer += arr[i+1]
+            elif l[i] == '-':
+                answer -= arr[i+1]
+            elif l[i] == '*':
+                answer *= arr[i+1]
+            else:
+                if answer < 0 and arr[i+1] > 0:
+                    answer = -(-answer // arr[i+1])
+                else:
+                    answer //= arr[i+1]
+    result.append(answer)
+
 print(max(result))
 print(min(result))
