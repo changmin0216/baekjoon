79f409194634a3e11a4b4991b1f02227f93456bf
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2024-12-24 21:45:00 +0900

    [level 3] Title: 입국심사, Time: 499.22 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=python3) 
 
 ### 성능 요약
 
-메모리: 91.2 MB, 시간: 75.63 ms
+메모리: 14.1 MB, 시간: 499.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 15:18:55
+2024년 12월 24일 21:44:55
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,19 @@
+def solution(n, times):
+    left = 1
+    right = max(times) * n
+    
+    while left <= right:
+        mid = (left+right)//2
+        cnt = 0
+     	
+        for time in times:
+            cnt += mid//time
+        
+            if cnt >= n:
+                break
+        if cnt >= n:
+            answer = mid
+            right = mid -1
+        else :
+            left = mid + 1
+    return answer
