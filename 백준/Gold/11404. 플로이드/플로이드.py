ab804232a36deb9b5077a52916fe6057abe2a36a
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-03-02 14:45:15 +0900

    [Gold IV] Title: 플로이드, Time: 156 ms, Memory: 111660 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,29 +1,30 @@
 import sys
 input = sys.stdin.readline
-INF = sys.maxsize
+INF = int(1e9)
 
-n = int(input()) #도시의 개수
-m = int(input()) #버스의 개수
+n = int(input())
+m = int(input())
 
-result = [[INF]*(n+1) for _ in range(n+1)]
-
-for i in range(0, n+1): 
-    result[i][i]=0
-
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    result[a][b] = min(result[a][b], c)
+graph = [[INF] * (n+1) for _ in range(n+1)]
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        for k in range(1, n+1):
-            if(result[k][j] > result[k][i] + result[i][j]):
-                result[k][j] = result[k][i] + result[i][j]
-        
-for i in range(1,n+1):
-    # if (i!=1):
-    #     print()
+        if i==j:
+            graph[i][j] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+for i in range(1, n+1):
     for j in range(1, n+1):
-        if result[i][j]==INF: print(0, end=' ' )
-        else: print(result[i][j], end=' ')
+        if graph[i][j]==INF:
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end=' ')
     print()
