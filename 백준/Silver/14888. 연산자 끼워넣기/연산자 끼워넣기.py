c72c668219e514f7b68d8e23a0202ee4c81b1a2b
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-03-07 19:03:57 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 244 ms, Memory: 114572 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,74 +1,40 @@
 import sys
-from itertools import permutations
 input = sys.stdin.readline
-#
-# n = int(input())
-# arr = list(map(int, input().split()))
-# op = list(map(int, input().split()))
-#
-# tmp = []
-#
-# o = ['+', '-', '*', '/']
-# for i in range(4):
-#     if op[i]!=0:
-#         for _ in range(op[i]):
-#             tmp.append(o[i])
-#
-# result = []
-# for l in permutations(tmp, len(tmp)):
-#     answer = 0
-#     for i in range(len(l)):
-#         if i==0:
-#             if l[i] == '+':
-#                 answer += arr[i]+arr[i+1]
-#             elif l[i] == '-':
-#                 answer += arr[i] - arr[i + 1]
-#             elif l[i] == '*':
-#                 answer += arr[i] * arr[i + 1]
-#             else:
-#                 if arr[i] < 0 and arr[i+1] > 0:
-#                     answer += -(-arr[i] // arr[i+1])
-#                 else:
-#                     answer += arr[i] // arr[i + 1]
-#         else:
-#             if l[i] == '+':
-#                 answer += arr[i+1]
-#             elif l[i] == '-':
-#                 answer -= arr[i+1]
-#             elif l[i] == '*':
-#                 answer *= arr[i+1]
-#             else:
-#                 if answer < 0 and arr[i+1] > 0:
-#                     answer = -(-answer // arr[i+1])
-#                 else:
-#                     answer //= arr[i+1]
-#     result.append(answer)
-#
-# print(max(result))
-# print(min(result))
 
 n = int(input())
 arr = list(map(int, input().split()))
-add, sub, mul, div = map(int, input().split())
+cal = list(map(int, input().split()))
 
 result = []
-def cal(add, sub, mul, div, answer, depth):
-    global result
+def recur(depth, sum_, idx):
+    if idx==0:
+        sum_+=arr[depth]
+    elif idx==1:
+        sum_ -= arr[depth]
+    elif idx==2:
+        sum_ *= arr[depth]
+    else:
+        if sum_ < 0 and arr[depth] > 0:
+            sum_ = -((-sum_) // arr[depth])
+        else:
+            sum_ //= arr[depth]
 
     if depth == n-1:
-        result.append(answer)
+        result.append(sum_)
         return
 
-    if add > 0:
-        cal(add-1, sub, mul, div, answer+arr[depth+1], depth+1)
-    if sub > 0:
-        cal(add, sub-1, mul, div, answer - arr[depth + 1], depth + 1)
-    if mul > 0:
-        cal(add, sub, mul-1, div, answer * arr[depth + 1], depth + 1)
-    if div > 0:
-        cal(add, sub, mul, div-1, int(answer / arr[depth + 1]), depth + 1)
+    for i in range(len(cal)):
+        if cal[i] != 0:
+            cal[i] -= 1
+            recur(depth+1, sum_, i)
+            cal[i] += 1
 
-cal(add, sub, mul, div, arr[0], 0)
+
+for i in range(len(cal)):
+    if cal[i] != 0:
+        cal[i]-=1
+        recur(1, arr[0], i)
+        cal[i]+=1
 
 print(max(result))
 print(min(result))
