4ade40fb140e2a926df419184e5a057cc26f1d98
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-03-12 14:56:25 +0900

    [Gold IV] Title: 행렬 제곱, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
@@ -2,36 +2,52 @@ import sys
 input = sys.stdin.readline
 
 n, b = map(int, input().split())
-a = []
+
+matrix = []
 for _ in range(n):
-    a.append(list(map(int, input().split())))
+    matrix.append(list(map(int, input().split())))
 
-def mul(u, v):
-    n = len(u)
-    z = [[0] * n for _ in range(n)]
+def multi_matrix(a, b):
+    x = [[0] * n for _ in range(n)]
 
-    for row in range(n):
-        for col in range(n):
-            e = 0
-            for i in range(n):
-                e += u[row][i] * v[i][col]
-            z[row][col] = e % 1000
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                x[i][j] += a[i][k]*b[k][j]
+            x[i][j]%=1000
 
-    return z
+    return x
+
+# if b == 1:
+#     for i in matrix:
+#         print(*i)
+# else:
+#     tmp = multi_matrix(matrix, matrix)
+#
+#     for _ in range(b-2):
+#         tmp = multi_matrix(tmp, matrix)
+#
+#     for i in range(n):
+#         for j in range(n):
+#             tmp[i][j] = tmp[i][j]%1000
+#
+#     for i in tmp:
+#         print(*i)
 
 def square(a, b):
-    if b == 1:
-        for x in range(len(a)):
-            for y in range(len(a)):
-                a[x][y] %= 1000
+    if b==1:
+        for i in range(n):
+            for j in range(n):
+                a[i][j]%=1000
         return a
 
-    tmp = square(a, b // 2)
-    if b % 2: ## 짝수
-        return mul(mul(tmp, tmp), a)
-    else: ## 홀수
-        return mul(tmp, tmp)
+    tmp = square(a, b//2)
+    if b%2==1: #짝수
+        return multi_matrix(multi_matrix(tmp, tmp), a)
+    else:
+        return multi_matrix(tmp, tmp)
 
-result = square(a, b)
-for r in result:
-    print(*r)
+result = square(matrix, b)
+
+for i in result:
+    print(*i)
