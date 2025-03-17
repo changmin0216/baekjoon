524fabeccdf2e3df21f9775b57562d15745469af
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-03-17 22:27:34 +0900

    [Silver II] Title: 이동하기, Time: 748 ms, Memory: 80592 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,33 +1,15 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
+dp = [[0]*(m+1) for _ in range(n+1)]
 
-graph = []
+g = []
 for _ in range(n):
-    graph.append(list(map(int, input().split())))
+    g.append(list(map(int, input().split())))
 
-dy = [1,0]
-dx = [0,1]
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = max(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + g[i-1][j-1]
 
-def bfs():
-    q = deque()
-    q.append((0,0))
-
-    visited = [[-1]*m for _ in range(n)]
-    visited[0][0] = graph[0][0]
-
-    while q:
-        ey, ex = q.popleft()
-
-        for i in range(2):
-            ny, nx = ey + dy[i], ex + dx[i]
-
-            if 0<=ny<n and 0<=nx<m:
-                if visited[ny][nx] < visited[ey][ex] + graph[ny][nx]:
-                    visited[ny][nx] = visited[ey][ex] + graph[ny][nx]
-                    q.append((ny, nx))
-    print(visited[n-1][m-1])
-
-bfs()
+print(dp[n][m])
