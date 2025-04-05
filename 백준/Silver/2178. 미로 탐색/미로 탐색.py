262ef928c9354e144de200ca6a7575eba2244a31
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-04-05 14:44:31 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,32 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
 
-N, M = map(int, input().split())
+n, m = map(int, input().split())
+graph = []
 
-maps = [[999] for _ in range(N+1)]
-for i in range(1, N+1):
-    for v in map(int, input().strip()):
-        maps[i].append(v)
+for i in range(n):
+    graph.append(list(map(int, input())))
 
-dx = [0,0,-1,1]
 dy = [-1,1,0,0]
-def bfs(y, x):
-    global maps
+dx = [0,0,-1,1]
+def bfs():
     q = deque()
-    q.append((y, x))
+    q.append((0, 0, 1))
+
+    visited = [[False]*m for _ in range(n)]
+    visited[0][0] = True
+
     while q:
-        ey, ex = q.popleft()
+        ey, ex, dist = q.popleft()
+
+        if ey==n-1 and ex==m-1:
+            print(dist)
+            return
         for i in range(4):
             ny, nx = ey + dy[i], ex + dx[i]
-            if 0 < ny <= N and 0 < nx <= M and maps[ny][nx] == 1:
-                    q.append((ny, nx))
-                    maps[ny][nx] = maps[ey][ex] + 1
-bfs(1,1)
-print(maps[N][M])
+
+            if 0<=ny<n and 0<=nx<m:
+                if graph[ny][nx]!=0 and not visited[ny][nx]:
+                    q.append((ny, nx, dist+1))
+                    visited[ny][nx] = True
+
+bfs()
