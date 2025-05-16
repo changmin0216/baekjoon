d681652ff4f67ca77bc8afe387be2a02b311cb41
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-05-16 16:45:52 +0900

    [Silver IV] Title: 설탕 배달, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,28 +1,43 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
+#
+# ## 3킬로그램과 5킬로그램
+# ## 3 ≤ N ≤ 5000
+#
+# n = int(input())
+#
+# if n<=5:
+#     if n==4:
+#         print(-1)
+#     else:
+#         print(1)
+# else:
+#     dp = [-1] * (n+1)
+#     dp[3] = 1
+#     dp[5] = 1
+#
+#     for i in range(6, n+1):
+#         if dp[i-3] == -1 and dp[i-5] == -1:
+#             continue
+#         if dp[i-3] != -1 and dp[i-5] != -1:
+#             dp[i] = min(dp[i-3], dp[i-5]) + 1
+#         elif dp[i-3] == -1:
+#             dp[i] = dp[i-5] + 1
+#         else:
+#             dp[i] = dp[i-3] + 1
+#     print(dp[n])
 
-## 3킬로그램과 5킬로그램
-## 3 ≤ N ≤ 5000
+num = int(input())
+count = 0
 
-n = int(input())
+while num >= 0:
+    if num % 5 == 0:
+        count += int(num // 5)
+        print(count)
+        break
 
-if n<=5:
-    if n==4:
-        print(-1)
-    else:
-        print(1)
+    num -= 3
+    count += 1
+
 else:
-    dp = [-1] * (n+1)
-    dp[3] = 1
-    dp[5] = 1
-
-    for i in range(6, n+1):
-        if dp[i-3] == -1 and dp[i-5] == -1:
-            continue
-        if dp[i-3] != -1 and dp[i-5] != -1:
-            dp[i] = min(dp[i-3], dp[i-5]) + 1
-        elif dp[i-3] == -1:
-            dp[i] = dp[i-5] + 1
-        else:
-            dp[i] = dp[i-3] + 1
-    print(dp[n])
+    print(-1)
