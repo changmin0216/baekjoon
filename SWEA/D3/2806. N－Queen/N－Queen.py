4ed9bbf70c430efcb9b503e9691b52774703ebf2
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-05-21 10:01:39 +0900

    [D3] Title: N-Queen, Time: 161 ms, Memory: 61,568 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,26 +1,29 @@
 def check(row):
     for i in range(row):
-        if column[i] == column[row] or row - i == abs(column[i] - column[row]):
+        if column[i] == column[row] or row - i == abs(column[row] - column[i]):
             return False
     return True
+
 def dfs(row):
     global result
     if row == n:
         result+=1
         return
-    
+
     for i in range(n):
         column[row] = i
         if check(row):
             dfs(row+1)
-
+    return
+####################
 t = int(input())
 
-for i in range(t):
+for case in range(t):
     n = int(input())
 
     column = [-1] * n
-    result = 0
 
+    result = 0
     dfs(0)
-    print(f'#{i+1} {result}')
+
+    print(f'#{case+1} {result}')
