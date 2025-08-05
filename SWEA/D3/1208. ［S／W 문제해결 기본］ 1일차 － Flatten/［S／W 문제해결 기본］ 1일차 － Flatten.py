9f4f83361e3509d01e18c06e756be9a0ee8ce5fb
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-08-05 11:12:27 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 77 ms, Memory: 59,392 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,15 +1,45 @@
-for i in range(10):
-    k = int(input())
+import java.io.*;
+import java.util.*;
 
-    arr = list(map(int, input().split()))
-    for _ in range(k):
-        max_index = arr.index(max(arr))
-        min_index = arr.index(min(arr))
+public class Solution {
 
-        if arr[max_index] - arr[min_index] <= 1:
-            break
-        arr[max_index] = arr[max_index]-1
-        arr[min_index] = arr[min_index]+1
-    print(f'#{i+1}', end=' ')
-    print(max(arr) - min(arr))
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		for (int tc=1;tc<=10;tc++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			int[] arr = new int[100];
+			st = new StringTokenizer(br.readLine());
+			for (int i=0;i<100;i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			
+			for (int i=0;i<n;i++) {
+				int max_index = 0;
+				int min_index = 0;
+				
+				for (int j=0;j<100;j++) {
+					if (arr[j]>arr[max_index]) {
+						max_index = j;
+					}
+					
+					else if(arr[j]<arr[min_index]) {
+						min_index = j;
+					}
+				}
+				
+				arr[max_index]-=1;
+				arr[min_index]+=1;
+				
+			}
+			int result = Arrays.stream(arr).max().getAsInt() - Arrays.stream(arr).min().getAsInt();
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
+		}
+		System.out.println(sb);
+	}
 
+}
