673b8b512df359381abe443ac9bc80e31b6815ab
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-08-06 10:54:53 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 107 ms, Memory: 60,672 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -1,56 +1,70 @@
-def solution(index):
-    rotate = 1
+import java.io.*;
+import java.util.*;
 
-    y = 0
-    x = index
+public class Solution {
+	static int[][] map;
+	static int[] dy = {-1,0,0};
+	static int[] dx = {0,-1,1};
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		for (int tc=1;tc<=10;tc++) {
+			int t = Integer.parseInt(br.readLine());
+			
+			map = new int[100][100];
+			int y=-1, x=-1;
+			for (int i=0;i<100;i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j=0;j<100;j++) {
+					int v = Integer.parseInt(st.nextToken());
+					map[i][j] = v;
+					if(v==2) {
+						y = i;
+						x = j;
+					}
+				}
+			}
+			
+			int d = 0; //상
+			while(y!=0) {
+				if (d == 0) { //현재 위를 바라보고 있으면
+					if (0<=x-1&&x-1<100) {
+						if (map[y][x-1]==1) {
+							d = 1;
+						}
+					}
+					
+					if (0<=x+1&&x+1<100) {
+						if (map[y][x+1]==1) {
+							d =2;
+						}
+					}
+				}
+				
+				else {
+					if (map[y-1][x] == 1) {
+						d = 0;
+					}
+				}
+				
+				y = y + dy[d];
+				x = x + dx[d];
+			}
+			sb.append("#").append(t).append(" ").append(x).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
 
-    while 0<=y<100 and 0<=x<100:
-        if graph[y][x] == 2:
-            return True
+//#1 67
+//#2 45
+//#3 39
+//#4 24
+//#5 91
+//#6 93
+//#7 90
+//#8 4
+//#9 99
+//#10 35
 
-        if rotate == 1:
-            for i in (2, 3):
-                ny, nx = y + dy[i], x+dx[i]
-
-                if 0<=ny<100 and 0<=nx<100:
-                    if graph[ny][nx] == 1:
-                        rotate = i
-                        y = ny
-                        x = nx
-                        break
-            else: ## 좌우로 못가면
-                y += dy[rotate]
-                x += dx[rotate]
-
-        else: ## 왼쪽
-            flag = False
-            ny = y + dy[1]
-            nx = x + dx[1]
-
-            if 0 <= ny < 100 and 0 <= nx < 100:
-                if graph[ny][nx] == 1:
-                    rotate = 1
-                    y = ny
-                    x = nx
-                    flag = True
-
-            if not flag:
-                y += dy[rotate]
-                x += dx[rotate]
-
-    return False
-dy = [-1,1,0,0]
-dx = [0,0,-1,1]
-for _ in range(10):
-    case = int(input())
-    
-    
-    graph = []
-    for _ in range(100):
-        graph.append(list(map(int, input().split())))
-
-    for i in range(100):
-        if graph[0][i] == 1:
-            if solution(i):
-                print(f'#{case} {i}')
-                break
