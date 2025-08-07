f208507c57a5bba74a51872496c97f90bcf55f0d
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-08-07 12:12:58 +0900

    [D3] Title: 햄버거 다이어트, Time: 749 ms, Memory: 26,752 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int n, l;
+	static int[] score;
+	static int[] cal;
+	static int R;
+	static int[] b;
+	static int result;
+	static void comb(int depth, int start, int calSum) {
+		if (calSum > l) return;
+		if(depth==R) {
+			int sum = 0;
+			for (int i=0;i<R;i++) {
+				sum+=b[i];
+			}
+			result = Math.max(result, sum);
+			return;
+		}
+		
+		for (int i=start;i<n;i++) {
+			b[depth] = score[i];
+			comb(depth+1, i+1, calSum+cal[i]);
+		}
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc=1;tc<=T;tc++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			l = Integer.parseInt(st.nextToken());
+			
+			score = new int[n];
+			cal = new int[n];
+			for (int i=0;i<n;i++) {
+				st = new StringTokenizer(br.readLine());
+				
+				score[i] = Integer.parseInt(st.nextToken());
+				cal[i] = Integer.parseInt(st.nextToken());				
+			}
+			
+			result = Integer.MIN_VALUE;
+			for (int i=1;i<n+1;i++) {
+				R = i;
+				b = new int[R]; 
+				comb(0, 0, 0);
+			}
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
