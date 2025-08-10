ae4b265bde2f38facfc03db52f26ad58e5be01cd
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-08-10 19:27:28 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 212 ms, Memory: 20048 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,63 +1,64 @@
+
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int[][] graph;
-    static int[][] dp;
+    static int n, tc;
+    static int[][] map;
     static int[] dy = {-1, 1, 0, 0};
     static int[] dx = {0, 0, -1, 1};
+    static void dijkstra() {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
+        pq.offer(new int[]{0, 0, map[0][0]});
 
-    static int bfs(){
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{0, 0});
+        int[][] dist = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+        dist[0][0] = map[0][0];
 
-        dp[0][0] = graph[0][0];
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
 
-        while (!q.isEmpty()) {
-            int[] now = q.poll();
+            if (dist[cur[0]][cur[1]] < cur[2]) {
+                continue;
+            }
 
             for (int i = 0; i < 4; i++) {
-                int ny = now[0] + dy[i];
-                int nx = now[1] + dx[i];
+                int ny = cur[0] + dy[i];
+                int nx = cur[1] + dx[i];
 
-                if (0 <= ny && ny < N && 0 <= nx && nx < N) {
-                    if (dp[ny][nx] == -1) {
-                        dp[ny][nx] = dp[now[0]][now[1]] + graph[ny][nx];
-                        q.offer(new int[]{ny, nx});
-                    } else if (dp[ny][nx] > dp[now[0]][now[1]] + graph[ny][nx]) {
-                        dp[ny][nx] = dp[now[0]][now[1]] + graph[ny][nx];
-                        q.offer(new int[]{ny, nx});
+                if (0 <= ny && ny < n && 0 <= nx && nx < n) {
+                    if (dist[ny][nx] > map[ny][nx] + cur[2]) {
+                        dist[ny][nx] = map[ny][nx] + cur[2];
+                        pq.offer(new int[]{ny, nx, dist[ny][nx]});
                     }
                 }
             }
         }
-        return dp[N-1][N-1];
+        System.out.printf("Problem %d: %d", tc, dist[n-1][n-1]);
+        System.out.println();
     }
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
 
-        int tc = 1;
+        tc = 1;
         while (true) {
-            N = Integer.parseInt(br.readLine());
-            if (N==0) break;
-
-            dp = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                Arrays.fill(dp[i], -1);
+            n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                break;
             }
-            graph = new int[N][N];
-            for (int i = 0; i < N; i++) {
+            map = new int[n][n];
+            for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    graph[i][j] = Integer.parseInt(st.nextToken());
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            sb.append("Problem ").append(tc).append(": ").append(bfs()).append("\n");
+            dijkstra();
             tc++;
         }
-        System.out.println(sb);
     }
 }
