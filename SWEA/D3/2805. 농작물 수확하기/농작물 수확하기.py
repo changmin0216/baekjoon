5906ccbbe2809b7c6d8cd4e0fbcc80c2ac61b245
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-08-11 17:03:10 +0900

    [D3] Title: 농작물 수확하기, Time: 117 ms, Memory: 62,336 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,30 +1,41 @@
-t = int(input())
+import java.io.*;
+import java.util.*;
 
-for case in range(t):
-    n = int(input())
+public class Solution {
+	static int n;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=0;tc<T;tc++) {
+			n = Integer.parseInt(br.readLine());
+			
+			int[][] map = new int[n][n];
+			for(int i=0;i<n;i++) {
+				String s = br.readLine();
+				for (int j=0;j<n;j++) {
+					map[i][j] = s.charAt(j) - '0';
+				}
+			}
+			int blank = n/2;
+			int sum = 0;
+			for (int i=0;i<n/2+1;i++) {
+				for (int j=blank;j<blank+(i*2+1);j++) {
+					sum+=map[i][j];
+				}
+				blank-=1;
+			}
+			blank = n/2;
+			for(int i=n-1;i>n/2;i--) {
+				for (int j=blank;j<blank+((n-1-i)*2+1);j++) {
+					sum+=map[i][j];
+				}
+				blank-=1;
+			}
+			sb.append("#").append(tc+1).append(" ").append(sum).append("\n");
+		}
+		System.out.println(sb);
+	}
 
-    graph = []
-    for _ in range(n):
-        graph.append(list(map(int, input())))
-
-    answer = 0
-    idx_left = n//2
-    idx_right = n//2 + 1
-    for i in range(n):
-        if i<n//2:
-            # for j in range(idx_left, idx_right):
-            #     print(graph[j])
-            #     answer+=graph[j]
-
-            answer+=sum(graph[i][idx_left:idx_right])
-            idx_left-=1
-            idx_right+=1
-        else:
-            answer += sum(graph[i][idx_left:idx_right])
-            # for j in range(idx_left, idx_right):
-            #     print(graph[j])
-            # 
-            #     answer+=graph[j]
-            idx_left += 1
-            idx_right -= 1
-    print(f'#{case+1} {answer}')
+}
