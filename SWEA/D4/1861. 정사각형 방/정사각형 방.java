70f00cb7408290c7b0a54c8fc5c7d823b9c3afd2
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-08-14 08:50:32 +0900

    [D4] Title: 정사각형 방, Time: 136 ms, Memory: 34,732 KB -BaekjoonHub

---
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int N;
+    static int[][] g;
+    static int[][] dp; 
+    static final int[] dy = {-1, 1, 0, 0};
+    static final int[] dx = {0, 0, -1, 1};
+
+    static int dfs(int y, int x) {
+        if (dp[y][x] != 0) return dp[y][x];  
+        dp[y][x] = 1; 
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (0 <= ny && ny < N && 0 <= nx && nx < N) {
+                if (g[ny][nx] == g[y][x] + 1) {
+                    dp[y][x] = 1 + dfs(ny, nx);
+                    break; 
+                }
+            }
+        }
+        return dp[y][x];
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine().trim());
+        for (int tc = 1; tc <= T; tc++) {
+            N = Integer.parseInt(br.readLine().trim());
+            g = new int[N][N];
+            dp = new int[N][N];
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    g[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            
+            int result_num = -1;
+            int result_cnt = Integer.MIN_VALUE;
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    int cnt = dfs(i, j);
+                    if (cnt>result_cnt || (cnt == result_cnt && g[i][j] < result_num)) {
+                    	result_cnt = cnt;
+                    	result_num = g[i][j];
+                    }
+                }
+            }
+
+            sb.append("#").append(tc).append(" ").append(result_num).append(" ").append(result_cnt).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
