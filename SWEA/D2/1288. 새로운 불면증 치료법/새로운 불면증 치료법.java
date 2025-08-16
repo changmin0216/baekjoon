abfaec5d18a90025171ffc5a27252babe46275f6
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-08-16 17:17:45 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 80 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class Solution{
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -11,8 +11,8 @@ public class Solution {
         int total = (1 << 10) -1;  // 모든 숫자가 등장했을 때의 값
         for (int tc=1;tc<=T;tc++) {
             int N = Integer.parseInt(br.readLine());
-//            int visited = 0;
-            boolean[] visited = new boolean[10];
+            int visited = 0;
+//            boolean[] visited = new boolean[10];
             int count = 0;
 
             for (count = 1; ; count++) {
@@ -20,14 +20,15 @@ public class Solution {
                 for( char c :arr){
                     int num = c - '0';
 //                    visited = visited | (1 << num);  // 각 숫자에 대해 등장했다는 의미로 bit 를 1로 변경
-
-                    visited[num] = true;
+                    visited = visited | (1 << num);
+//                    visited[num] = true;
                 }
-                boolean flag = true;
-                for (int k=0;k<=9;k++){
-                    if (visited[k] == false) flag = false;
-                }
-                if (flag) break;
+                if (visited == total) break;
+//                boolean flag = true;
+//                for (int k=0;k<=9;k++){
+//                    if (visited[k] == false) flag = false;
+//                }
+//                if (flag) break;
             }
             System.out.println("#" + tc + " " + N*count);
         }
