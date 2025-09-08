30c0637305105b8b2fce77b8f916e93acc22b2e2
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-09-08 16:57:09 +0900

    [D3] Title: 0/1 Knapsack, Time: 101 ms, Memory: 32,440 KB -BaekjoonHub

---
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -0,0 +1,41 @@
+
+import java.io.*;
+import java.util.*;
+public class Solution{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());	
+		for (int tc=1;tc<=T;tc++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			
+			int[] v = new int[n+1];
+			int[] c = new int[n+1];
+			
+			for (int i=1;i<=n;i++) {
+				st = new StringTokenizer(br.readLine());
+				v[i] = Integer.parseInt(st.nextToken());
+				c[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int[][] dp = new int[n+1][k+1];
+			
+			for (int i=1;i<=n;i++) {
+				for (int j=0;j<=k;j++) {
+					if (v[i]>j) dp[i][j] = dp[i-1][j];
+					else {
+						dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-v[i]]+c[i]);
+					}
+				}
+			}
+			
+			sb.append("#").append(tc).append(" ").append(dp[n][k]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
