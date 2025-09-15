6986a8e7c5543fb4e65fa4ab8642e3f71cacb420
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-09-15 16:42:08 +0900

    [D3] Title: 퍼펙트 셔플, Time: 114 ms, Memory: 31,104 KB -BaekjoonHub

---
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
@@ -0,0 +1,42 @@
+
+import java.io.*;
+import java.util.*;
+public class Solution{
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		ArrayDeque<String> q1 = new ArrayDeque<>();
+		ArrayDeque<String> q2 = new ArrayDeque<>();
+		for (int tc=1;tc<=T;tc++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			st = new StringTokenizer(br.readLine());
+			if (n%2==0) {
+				for (int i=0;i<n;i++) {
+					if (i < n/2) q1.offer(st.nextToken());
+					else q2.offer(st.nextToken());				
+				}
+			} 
+			else {
+				for (int i=0;i<n;i++) {
+					if (i <= n/2) q1.offer(st.nextToken());
+					else q2.offer(st.nextToken());				
+				}
+			}
+			
+			
+			sb.append("#").append(tc).append(" ");
+			for (int i=0;i<n;i++) {
+				if (i%2==0) sb.append(q1.poll()).append(" ");
+				else sb.append(q2.poll()).append(" ");				
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
