29b5984dde7a902506a12354710698c0c5597db3
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-09-15 11:20:37 +0900

    [Gold V] Title: 최소비용 구하기, Time: 640 ms, Memory: 54120 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,69 +1,77 @@
-
-
 import java.io.*;
 import java.util.*;
-
 public class Main {
-    static class Node implements Comparable<Node> {
-        int vertex, weight;
-
-        Node(int vertex, int weight) {
-            this.vertex = vertex;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Node other) {
-            return Integer.compare(this.weight, other.weight);  // 가중치 기준 오름차순 정렬
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-
-        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            graph.get(a).add(new Node(b, c));
-        }
-        st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
-        dijkstra(graph, start, end, n);
-    }
-
-    static void dijkstra(ArrayList<ArrayList<Node>> graph, int start, int end, int n) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0;
-
-        pq.add(new Node(start, 0));
-
-        while (!pq.isEmpty()) {
-            Node current = pq.poll();
-            int now = current.vertex;
-            int weight = current.weight;
-
-            if (weight > dist[now]) continue;  // 이미 처리된 경우 건너뜀
-
-            for (Node neighbor : graph.get(now)) {
-                int cost = dist[now] + neighbor.weight;
-                if (cost < dist[neighbor.vertex]) {
-                    dist[neighbor.vertex] = cost;
-                    pq.add(new Node(neighbor.vertex, cost));
-                }
-            }
-        }
-        System.out.println(dist[end]);
-    }
+	static int n, m, start, end; 
+	static int[][] g;
+	public static void main(String[] args) throws Exception {
+		//-------여기에 해결 코드를 작성하세요.
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		
+		g = new int[n+1][n+1];
+		for (int i=0;i<n+1;i++) {
+			for (int j=0;j<n+1;j++) {
+				if (i==j) {
+					g[i][j] = 0;
+				}
+				
+				else {
+//					g[i][j] = Integer.MAX_VALUE;
+					g[i][j] = -1;
+				}
+			}
+		}
+		
+		for (int i=0;i<m;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			if (g[a][b]==-1) {
+				g[a][b] = c;
+			} else {
+				g[a][b] = Math.min(g[a][b], c);
+			}
+//			g[a][b] = Math.min(g[a][b], c);
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		start = Integer.parseInt(st.nextToken());
+		end = Integer.parseInt(st.nextToken());
+		System.out.println(dijkstra());
+	}
+	
+	static int dijkstra() {
+		int[] dist = new int[n+1];
+		
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		
+		dist[start] = 0;
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[0]-o2[0]);
+		pq.offer(new int[] {0, start});
+		
+		while(!pq.isEmpty()) {
+			int[] now = pq.poll();
+			
+			if (dist[now[1]] < now[0]) continue;
+			for (int i=1;i<n+1;i++) {
+//				if (g[now[1]][i]!=Integer.MAX_VALUE) {
+//					if (dist[i] > now[0] + g[now[1]][i]) {
+//						dist[i] = now[0] + g[now[1]][i];
+//						pq.offer(new int[] {dist[i], i});
+//					}
+//				}
+				if (g[now[1]][i]==-1) continue;
+				if (dist[i] > now[0] + g[now[1]][i]) {
+					dist[i] = now[0] + g[now[1]][i];
+					pq.offer(new int[] {dist[i], i});
+				}
+			}
+		}
+		return dist[end];
+	}
 }
