df9ccf2b8b3ce6c2fb8e2bdd18b0b115485db029
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-09-15 17:14:12 +0900

    [D3] Title: 퍼펙트 셔플, Time: 98 ms, Memory: 30,208 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
@@ -1,40 +1,35 @@
 import java.io.*;
 import java.util.*;
-public class Solution {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		
-		ArrayDeque<String> q1 = new ArrayDeque<>();
-		ArrayDeque<String> q2 = new ArrayDeque<>();
-		for (int tc=1;tc<=T;tc++) {
-			int n = Integer.parseInt(br.readLine());
-			
-			st = new StringTokenizer(br.readLine());
-			if (n%2==0) {
-				for (int i=0;i<n;i++) {
-					if (i < n/2) q1.offer(st.nextToken());
-					else q2.offer(st.nextToken());				
-				}
-			} 
-			else {
-				for (int i=0;i<n;i++) {
-					if (i <= n/2) q1.offer(st.nextToken());
-					else q2.offer(st.nextToken());				
-				}
-			}
-			
-			
-			sb.append("#").append(tc).append(" ");
-			for (int i=0;i<n;i++) {
-				if (i%2==0) sb.append(q1.poll()).append(" ");
-				else sb.append(q2.poll()).append(" ");				
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb);
-	}
+public class Solution{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int n = Integer.parseInt(br.readLine());
+            String[] cards = new String[n];
+
+            // 카드 n개 안전하게 읽기(줄바꿈 섞여도 OK)
+            int idx = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (idx < n) {
+                while (!st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+                cards[idx++] = st.nextToken();
+            }
+
+            int L = (n + 1) / 2; // 왼쪽 덱 크기
+            sb.append("#").append(tc).append(" ");
+
+            int i = 0;     // 왼쪽 인덱스 [0 .. L-1]
+            int j = L;     // 오른쪽 인덱스 [L .. n-1]
+            for (int k = 0; k < n; k++) {
+                if ((k & 1) == 0) sb.append(cards[i++]).append(" ");
+                else               sb.append(cards[j++]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
 }
