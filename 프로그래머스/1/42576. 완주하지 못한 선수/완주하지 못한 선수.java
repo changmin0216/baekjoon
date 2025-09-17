504c4774418c273ade305c6fbe32b286bf7cb026
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-09-18 08:52:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.86 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,8 +3,9 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         HashMap<String, Integer> map = new HashMap<>();
         for (String name : participant) {
-            if(!map.containsKey(name)) map.put(name, 1);
-            else map.put(name, map.get(name)+1);
+            map.put(name, map.getOrDefault(name, 0)+1);
+            // if(!map.containsKey(name)) map.put(name, 1);
+            // else map.put(name, map.get(name)+1);
         }
         
         for (String name : completion) {
