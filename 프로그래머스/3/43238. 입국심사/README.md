d5d5ee96d8c9a27f4a66753f9907f0f32f98514d
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-09-19 12:30:41 +0900

    [level 3] Title: 입국심사, Time: 58.87 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
 
 ### 성능 요약
 
-메모리: 14.1 MB, 시간: 499.22 ms
+메모리: 95.4 MB, 시간: 58.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 24일 21:44:55
+2025년 09월 19일 12:30:38
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,28 +1,28 @@
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
+        long max = 0;
+        for (int t : times) max = Math.max(max, t);
 
-        long l = n;
-
-        long max = -1;
-        for (int i = 0; i < times.length; i++) {
-            max = Math.max(max, times[i]);
-        }
-
-        long left = 1;
-        long right = l*max;
+        long left = 1;                  // 최소 1분
+        long right = max * (long) n;    // 최댓값 * 인원수 (long 캐스팅!)
+        long answer = right;
 
         while (left <= right) {
-            long mid = (left+right)/2;
-            long count = 0;
-            for (int i = 0; i < times.length; i++) {
-                count += mid / times[i];
+            long mid = left + (right - left) / 2;  // 오버플로우 방지
+            long cnt = 0;
+
+            for (int t : times) {
+                cnt += mid / t;
+                if (cnt >= n) {        // 포화 합산으로 오버플로우 방지
+                    break;
+                }
             }
-            if (count >= n) {
+
+            if (cnt < n) {
+                left = mid + 1;
+            } else {
                 answer = mid;
                 right = mid - 1;
-            } else {
-                left = mid + 1;
             }
         }
         return answer;
