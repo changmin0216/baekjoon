a09e1264fe11bbe896491bb36f5773ea3b8e02ea
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-09-19 12:31:06 +0900

    [level 3] Title: 입국심사, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,30 +1,26 @@
 class Solution {
     public long solution(int n, int[] times) {
-        long max = 0;
-        for (int t : times) max = Math.max(max, t);
-
-        long left = 1;                  // 최소 1분
-        long right = max * (long) n;    // 최댓값 * 인원수 (long 캐스팅!)
-        long answer = right;
-
-        while (left <= right) {
-            long mid = left + (right - left) / 2;  // 오버플로우 방지
+        long answer = 0;
+        
+        long left = 0;
+        long right = Integer.MAX_VALUE;
+        
+        while(left<=right) {
+            long mid = (left + right) / 2;
+            
             long cnt = 0;
-
-            for (int t : times) {
-                cnt += mid / t;
-                if (cnt >= n) {        // 포화 합산으로 오버플로우 방지
-                    break;
-                }
+            for (int i=0;i<times.length;i++) {
+                cnt+=mid/times[i];
             }
-
-            if (cnt < n) {
-                left = mid + 1;
-            } else {
+            
+            if (cnt<n) left = mid+1;
+            else {
                 answer = mid;
                 right = mid - 1;
+        
             }
         }
+        
         return answer;
     }
 }
